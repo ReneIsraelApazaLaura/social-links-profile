ff1d6e8284d5bb8df6c793165bb84e20dc48d245
israelapaza <apaza.6924406@gmail.com>
2025-03-30 12:04:50 -0400

    correcciones

---
+++ index.js
@@ -0,0 +1,12 @@
+//console.log("hola amigos")
+//function sumar(numeroUno, numeroDos) {
+//    let resultado = numeroUno + numeroDos;
+//    return resultado;
+//}
+const profile = document.createElement("section")
+const main = document.querySelector("main")
+main.appendChild(profile)
+const header = document.createElement("header")
+profile.appendChild(header)
+const imagen = document.createElement("Img")
+header.appendChild(imagen)
